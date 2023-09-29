8dd56b7cad3fda0160249a42739798f8efd1eea0
이은우 <80537533+lew0205@users.noreply.github.com>
2023-09-29 10:39:32 +0900

    [ADD] getMenuTest

--- src/test/java/com/project/springapistudy/domain/menu/MenuServiceTest.java
+++ src/test/java/com/project/springapistudy/domain/menu/MenuServiceTest.java
@@ -1,20 +1,22 @@
 package com.project.springapistudy.domain.menu;
 
 import com.project.springapistudy.domain.menu.dto.req.RegisterMenuReqDto;
+import com.project.springapistudy.domain.menu.dto.res.GetMenuResDto;
 import com.project.springapistudy.domain.menu.exception.MenuNotFoundException;
 import com.project.springapistudy.domain.menu.repository.MenuRepository;
 import com.project.springapistudy.domain.menu.service.GetMenuService;
 import com.project.springapistudy.domain.menu.service.RegisterMenuService;
-import org.hibernate.annotations.Comment;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.transaction.Transactional;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.SoftAssertions.*;
 
 @SpringBootTest
 @Transactional
@@ -28,7 +30,7 @@ public class MenuServiceTest {
     private GetMenuService getMenuService;
 
     @Order(0)
-    @Comment("메뉴를 주문한다.")
+    @DisplayName("메뉴를 주문한다.")
     @Test
     void registerMenu() {
         //given
@@ -41,7 +43,29 @@ public class MenuServiceTest {
 
         //then
         Menu findMenu = menuRepository.findAll().stream().findFirst().orElseThrow(MenuNotFoundException::new);
-        assertThat(findMenu.getName()).isEqualTo(menuName);
-        assertThat(findMenu.getPrice()).isEqualTo(menuPrice);
+        assertSoftly(softAssertions -> {
+            assertThat(findMenu.getName()).isEqualTo(menuName);
+            assertThat(findMenu.getPrice()).isEqualTo(menuPrice);
+        });
+    }
+
+    @DisplayName("메뉴를 조회한다.")
+    @Test
+    void getMenu() {
+        //given
+        String menuName = "testMenu";
+        Long menuPrice = 1234L;
+        RegisterMenuReqDto reqDto = new RegisterMenuReqDto(menuName, menuPrice);
+        registerMenuService.execute(reqDto); // 서비스 테스트에서 다른 서비스를 호출하는게 맞는것인가?
+        Long id = menuRepository.findAll().stream().findFirst().orElseThrow().getId();
+
+        //when
+        GetMenuResDto findMenu = getMenuService.execute(id);
+
+        //then
+        assertSoftly(softAssertions -> {
+            assertThat(findMenu.getName()).isEqualTo(menuName);
+            assertThat(findMenu.getPrice()).isEqualTo(menuPrice);
+        });
     }
 }
